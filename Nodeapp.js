663bb5e6ac32b3a43f6ec5636aadeedf12611434
pranjal-walunj <pranjalwalunj132@gmail.com>
2024-12-29 12:17:54 +0530

    Update Nodeapp.js

--- Nodeapp.js
+++ Nodeapp.js
@@ -35,17 +35,3 @@ RUN npm install
 COPY . .
 EXPOSE 3000
 CMD ["node","index.js"]
-
-
-
-# Instructions to Use:
-
--To build Dockerfile :
-docker build -t imgname .
--To check whether image is created using Dockerfile :
-docker images
--To launch a container using image of this Dockerfile :
-docker run -d -p80:3000 --name nodecontainer imgname
--To check whether container is created using image :
-docker ps
--Here the port 80 is port of EC2 instance whereas the port 3000 is port of Nodejs container
